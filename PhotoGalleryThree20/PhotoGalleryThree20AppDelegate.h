d91950e6094a504b5d4fcaf1a233523e74272dd4
Phillip Verheyden <pverheyden@gmail.com>
2011-06-14 14:33:52 -0500

    Initial Commit

---
+++ PhotoGalleryThree20/PhotoGalleryThree20AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  PhotoGalleryThree20AppDelegate.h
+//  PhotoGalleryThree20
+//
+//  Created by Phillip Verheyden on 6/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PhotoGalleryThree20ViewController;
+
+@interface PhotoGalleryThree20AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet PhotoGalleryThree20ViewController *viewController;
+
+@end
